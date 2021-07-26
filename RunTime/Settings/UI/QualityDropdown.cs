f36145d110caf68bd6d8cee29a9f558743fe376a
Martin Parm <parmus@gmail.com>
2021-07-26 13:40:49 +0200

    Add support for controlling video settings

---
+++ RunTime/Settings/UI/QualityDropdown.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Games.GrumpyBear.Core.Settings.UI
+{
+    [AddComponentMenu("Grumpy Bear Games/Core/Settings/Quality Dropdown")]
+    [RequireComponent(typeof(Dropdown))]
+    public class QualityDropdown : MonoBehaviour
+    {
+        [SerializeField] private VideoSettings _videoSettings;
+
+        private Dropdown _dropdown;
+
+        private void Awake() => _dropdown = GetComponent<Dropdown>();
+
+        private void OnEnable()
+        {
+            _dropdown.ClearOptions();
+            _dropdown.AddOptions(QualityNames);
+            _dropdown.value = _videoSettings.QualityIndex;
+            _dropdown.RefreshShownValue();
+            
+            _dropdown.onValueChanged.AddListener(OnValueChange);
+        }
+
+        private void OnDisable() => _dropdown.onValueChanged.RemoveListener(OnValueChange);
+
+        private void OnValueChange(int qualityIndex) => _videoSettings.QualityIndex = qualityIndex;
+
+        public static List<string> QualityNames => QualitySettings.names.ToList();
+    }
+}
---
+++ RunTime/Settings/VideoSettings.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace Games.GrumpyBear.Core.Settings
+{
+    [CreateAssetMenu(menuName = "Grumpy Bear Games/Core/Settings/Video Settings", fileName = "Video Settings")]
+    public class VideoSettings: ScriptableObject
+    {
+        [SerializeField] private string _fullscreenSettingsKey = "Settings/Video/Fullscreen";
+        [SerializeField] private string _qualityIndexSettingsKey = "Settings/Video/QualityIndex";
+        [SerializeField] private string _resolutionIndexSettingsKey = "Settings/Video/ResolutionIndex";
+
+        public bool Fullscreen
+        {
+            get => PlayerPrefs.GetInt(_fullscreenSettingsKey, Screen.fullScreen ? 1 : 0) != 0;
+            set
+            {
+                Screen.fullScreen = value;
+                PlayerPrefs.SetInt(_fullscreenSettingsKey, value ? 1 : 0);
+            }
+        }
+
+        public int QualityIndex
+        {
+            get => PlayerPrefs.GetInt(_qualityIndexSettingsKey, QualitySettings.GetQualityLevel());
+            set
+            {
+                QualitySettings.SetQualityLevel(value);
+                PlayerPrefs.SetInt(_qualityIndexSettingsKey, value);
+            }
+        }
+
+        public ResolutionEntry Resolution
+        {
+            get => ResolutionEntry.FromString(PlayerPrefs.GetString(_resolutionIndexSettingsKey));
+            set
+            {
+                Screen.SetResolution(value.Width, value.Height, Fullscreen);
+                PlayerPrefs.SetString(_resolutionIndexSettingsKey, value.ToString());
+            }
+        }
+
+        private void ApplyAll()
+        {
+            Screen.fullScreen = Fullscreen;
+            QualitySettings.SetQualityLevel(QualityIndex);
+            var res = Resolution;
+            Screen.SetResolution(res.Width, res.Height, Fullscreen);
+        }
+
+        public List<ResolutionEntry> Resolutions
+        {
+            get
+            {
+                var result =
+                    new HashSet<ResolutionEntry>(Screen.resolutions.Select(ResolutionEntry.FromResolution))
+                        .ToList();
+                result.Sort();
+                return result;
+            }
+        }
+
+        public class ResolutionEntry : IComparable<ResolutionEntry>
+        {
+            public readonly int Height;
+
+            public readonly int Width;
+
+            internal ResolutionEntry(int width, int height)
+            {
+                Height = height;
+                Width = width;
+            }
+            
+            internal static ResolutionEntry FromResolution(Resolution resolution) => new ResolutionEntry(resolution.width, resolution.height);
+
+            internal static ResolutionEntry FromString(string resolution)
+            {
+                try
+                {
+                    var entries = resolution.Split(new[] {'x'}, 2);
+                    return new ResolutionEntry(int.Parse(entries[0]), int.Parse(entries[1]));
+                }
+                catch (Exception)
+                {
+                    return new ResolutionEntry(Screen.currentResolution.width, Screen.currentResolution.height);
+                }
+            }
+
+            public override string ToString() => $"{Width}x{Height}";
+
+            public int CompareTo(ResolutionEntry other)
+            {
+                var result = Width.CompareTo(other.Width);
+                return result != 0 ? result : Height.CompareTo(other.Height);
+            }
+
+            public override bool Equals(object other)
+            {
+                if (!(other is ResolutionEntry entry)) return false; 
+                return Height == entry.Height && Width == entry.Width;
+            }
+
+            public override int GetHashCode() => unchecked((Height * 397) ^ Width);
+        }
+
+        #if UNITY_EDITOR
+        private void OnEnable()
+        {
+            UnityEditor.EditorApplication.playModeStateChanged += change =>
+            {
+                if (change != UnityEditor.PlayModeStateChange.EnteredPlayMode) return;
+                ApplyAll();
+            };
+        }
+
+        [ContextMenu("Clear PlayerPrefs")]
+        private void ClearPlayerPrefs()
+        {
+            PlayerPrefs.DeleteKey(_fullscreenSettingsKey);
+            PlayerPrefs.DeleteKey(_qualityIndexSettingsKey);
+            PlayerPrefs.DeleteKey(_resolutionIndexSettingsKey);
+        }
+        #else
+        private void OnEnable() => ApplyAll();
+        #endif
+    }
+}
