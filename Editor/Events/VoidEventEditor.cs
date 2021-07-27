54f12bd0e0b5196383615543db7ccab7c8c7d039
Martin Parm <parmus@gmail.com>
2021-07-27 11:10:09 +0200

    Add custom editor for VoidEvent

---
+++ Editor/Events/VoidEventEditor.cs
@@ -0,0 +1,20 @@
+﻿using Games.GrumpyBear.Core.Events;
+using UnityEditor;
+using UnityEngine;
+
+namespace Games.GrumpyBear.Core.Editor.Events
+{
+    [CustomEditor(typeof(VoidEvent))]
+    public class VoidEventEditor : UnityEditor.Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+
+            if (!Application.isPlaying) return;
+            GUILayout.Space(20);
+            if (!GUILayout.Button("Trigger")) return;
+            if (target is VoidEvent voidEvent) voidEvent.Invoke();
+        }
+    }
+}
