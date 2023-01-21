7daaff52c1b711e1af2aa70453ca585b45a66571
Martin Parm <parmus@gmail.com>
2023-01-21 16:09:21 +0100

    Add useful helper-methods to IntObservable

--- RunTime/Observables/ScriptableObjects/IntObservable.cs
+++ RunTime/Observables/ScriptableObjects/IntObservable.cs
@@ -3,5 +3,10 @@
 namespace Games.GrumpyBear.Core.Observables.ScriptableObjects
 {
     [CreateAssetMenu(menuName = "Grumpy Bear Games/Core/Observables/Int Observable", fileName = "Int Observable")]
-    public class IntObservable: Observable<int> { }
+    public class IntObservable : Observable<int>
+    {
+        public void Add(int x) => Set(Value + x);
+        public void Increment() => Add(1);
+        public void Decrement() => Add(-1);
+    }
 }
