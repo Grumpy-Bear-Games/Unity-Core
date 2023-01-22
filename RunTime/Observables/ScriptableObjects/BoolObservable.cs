185749aebccd25519e428c7bb01c3b0bfe4c554d
Martin Parm <parmus@gmail.com>
2023-01-22 18:59:07 +0100

    Add Toggle() to BoolObservable

--- RunTime/Observables/ScriptableObjects/BoolObservable.cs
+++ RunTime/Observables/ScriptableObjects/BoolObservable.cs
@@ -3,5 +3,8 @@
 namespace Games.GrumpyBear.Core.Observables.ScriptableObjects
 {
     [CreateAssetMenu(menuName = "Grumpy Bear Games/Core/Observables/Bool Observable", fileName = "Bool Observable")]
-    public class BoolObservable: Observable<bool> { }
+    public class BoolObservable : Observable<bool>
+    {
+        public void Toggle() => Set(!Value);
+    }
 }
