d8b1948a1a44b9e58230e290849878c1570b296b
Martin Parm <parmus@gmail.com>
2023-03-01 00:19:27 +0100

    Thorough documentation of Events namespace

--- RunTime/Events/VoidEventListener.cs
+++ RunTime/Events/VoidEventListener.cs
@@ -3,10 +3,25 @@ using UnityEngine.Events;
 
 namespace Games.GrumpyBear.Core.Events
 {
+    /// <summary>
+    /// Companion helper class to <see cref="VoidEvent"/> to call methods on other component,
+    /// when the event trigger. This is similar to <see cref="EventTListener{T}"/> but without
+    /// an event payload.
+    /// 
+    /// </summary>
+    /// <seealso cref="VoidEvent"/>
     [AddComponentMenu("Grumpy Bear Games/Core/Events/Void Event Listener")]
     public class VoidEventListener : MonoBehaviour
     {
+        /// <summary>
+        /// The <see cref="VoidEvent"/> which this component should subscribe to.
+        /// </summary>
         [SerializeField] protected VoidEvent _event;
+        
+        /// <summary>
+        /// The <c>UnityEvent</c> which gets invoked,
+        /// when this component receives an event.
+        /// </summary>
         [SerializeField] protected UnityEvent _onTrigger;
 
         private void OnEnable() => _event.Listeners += OnTrigger;
